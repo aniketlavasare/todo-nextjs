4e01c5812ab093af9efa85cb9a2cd43c26086c3f
aavasare <aniketavasare2004@gmail.com>
2024-01-10 22:10:44 -0600

    progress page ui first

--- app/dashboard/statistics/page.tsx
+++ app/dashboard/statistics/page.tsx
@@ -1,7 +1,13 @@
+import ProgressCard from "@/app/ui-components/progress/progress-card";
+
 export default function Statistics () {
     return (
-    <div>
-        this is the progress page
+    <div className="flex flex-col gap-4">
+    <div className="flex flex-row justify-around">
+       <ProgressCard title={"Tasks Completed"} value={"07"} placeholder={"View All"}/>
+       <ProgressCard title={"Tasks Pending"} value={"07"} placeholder={"View All"}/>
+    </div>
+    <div className="bg-zinc-300 rounded-md cursor-pointer flex flex-row justify-center text-black w-full h-12 text-lg"><button>Set Target</button></div>
     </div>
     );
 }
---
+++ app/ui-components/progress/progress-card.tsx
@@ -0,0 +1,9 @@
+export default function ProgressCard({title, value, placeholder} : {title: string, value: number | string, placeholder: string}) {
+    return(
+        <div className="flex flex-col justify-center items-center h-auto border-2 rounded-md p-4">
+            <div className="text-2xl">{title}</div>
+            <div className=" text-8xl mx-20 my-14">{value}</div>
+            <div className="bg-zinc-300 rounded-md cursor-pointer flex flex-row justify-center text-black w-full h-8 text-lg"><button>{placeholder}</button></div>
+        </div>
+    );
+}
