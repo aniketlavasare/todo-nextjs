ab98f6ddeef1b4f677de305ecc8643be8a62e160
aavasare <aniketavasare2004@gmail.com>
2024-05-26 21:11:36 -0500

    new branch

--- app/ui-components/progress/progress-card.tsx
+++ app/ui-components/progress/progress-card.tsx
@@ -1,11 +1,11 @@
 import { redirect } from "next/navigation";
 
-export default function ProgressCard({title, value, placeholder} : {title: string, value: number | string, placeholder: string}) {
+export default function ProgressCard({title, value, placeholder, link} : {title: string, value: number | string, placeholder: string, link:string}) {
     return(
         <div className="flex flex-col justify-center items-center h-auto border-2 rounded-md p-4">
             <div className="text-2xl">{title}</div>
             <div className=" text-8xl mx-20 my-14">{value}</div>
-            <div className="bg-zinc-300 rounded-md cursor-pointer flex flex-row justify-center text-black w-full h-8 text-lg"><button>{placeholder}</button></div>
+            <div className="bg-zinc-300 rounded-md cursor-pointer flex flex-row justify-center text-black w-full h-8 text-lg"><button onClick={redirect(link)}>{placeholder}</button></div>
         </div>
     );
 }
