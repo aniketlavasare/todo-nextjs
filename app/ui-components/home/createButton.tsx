705c880fc94b9a3521d089363fd2df1d54b13b31
aavasare <aniketavasare2004@gmail.com>
2024-05-26 21:34:27 -0500

    tried some changes on branch

--- app/ui-components/home/createButton.tsx
+++ app/ui-components/home/createButton.tsx
@@ -1,11 +1,12 @@
 import { Plus } from "react-feather";
 import Link from "next/link";
 
-export default function CreateButton({status,setCreate }: {status : boolean, setCreate: React.Dispatch<React.SetStateAction<boolean>>}) {
+
+export default function CreateButton() {
     return(
         
-        <button onClick={()=>{setCreate(!status)}} className=" bg-zinc-300 rounded-md cursor-pointer flex flex-row justify-center mt-2 h-6 w-full">
+        <Link href='\create' className=" bg-zinc-300 rounded-md cursor-pointer flex flex-row justify-center mt-2 h-6 w-full">
             <Plus className="text-black"/>
-        </button>
+        </Link>
     );
 }
